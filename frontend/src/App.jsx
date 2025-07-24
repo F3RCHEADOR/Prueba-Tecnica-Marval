23a5df9e1791d77eb4948a9c11639c43b793c7d8
Fabian F <noobseca123@hotmail.com>
2025-07-23 23:38:07 -0500

    hot fix login ruta

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate, useLocation } from "react-router-dom";
 import Header from "./components/modules/Header";
 import Footer from "./components/modules/Footer";
 import List from "./pages/List";
@@ -7,6 +7,15 @@ import Login from "./pages/Login";
 import Details from "./pages/details";
 import Proyectos from "./pages/Proyectos";
 import { useState, useEffect } from "react";
+
+// Componente para proteger rutas
+function ProtectedRoute({ userInfo, children }) {
+  const location = useLocation();
+  if (!userInfo || !userInfo.username) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+  return children;
+}
 
 function App() {
   const [userInfo, setUserInfo] = useState({});
@@ -21,24 +30,44 @@ function App() {
       <div className="container mx-auto px-4">
         <div className="min-h-screen">
           <Routes>
-            <Route path="/" element={<List />} />
-            <Route
-              path="/form"
-              element={
-                <FormPage setUserInfo={setUserInfo} userInfo={userInfo} />
-              }
-            />
             <Route
               path="/login"
               element={<Login setUserInfo={setUserInfo} userInfo={userInfo} />}
             />
             <Route
-              path="/details/:id"
+              path="/"
               element={
-                <Details setUserInfo={setUserInfo} userInfo={userInfo} />
+                <ProtectedRoute userInfo={userInfo}>
+                  <List />
+                </ProtectedRoute>
               }
             />
-            <Route path="/proyectos" element={<Proyectos />} />
+            <Route
+              path="/form"
+              element={
+                <ProtectedRoute userInfo={userInfo}>
+                  <FormPage setUserInfo={setUserInfo} userInfo={userInfo} />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/details/:id"
+              element={
+                <ProtectedRoute userInfo={userInfo}>
+                  <Details setUserInfo={setUserInfo} userInfo={userInfo} />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/proyectos"
+              element={
+                <ProtectedRoute userInfo={userInfo}>
+                  <Proyectos />
+                </ProtectedRoute>
+              }
+            />
+            {/* Redirección por defecto */}
+            <Route path="*" element={<Navigate to="/login" replace />} />
           </Routes>
         </div>
       </div>
